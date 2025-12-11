7bf134b053470e72a1eb28d44eebed6371eb73f1
VladyslavKozlov <vladislavkozlov2101@gmail.com>
2025-12-11 14:40:44 +0200

    feat: add HeadComponent for importing Google Fonts in emails

--- emails/aq-components/HeadComponent.tsx
+++ emails/aq-components/HeadComponent.tsx
@@ -9,29 +9,9 @@ const HeadComponent = () => {
         href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"
         rel="stylesheet"
       />
-
-      {/* Open Sans Regular */}
-      <Font
-        fontFamily="Open Sans"
-        fallbackFontFamily="Arial"
-        webFont={{
-          url: 'https://fonts.gstatic.com/s/opensans/v40/mem8YaGs126MiZpBA-U1UpcaXcl0Aw.woff2',
-          format: 'woff2',
-        }}
-        fontWeight={400}
-        fontStyle="normal"
-      />
-
-      {/* Open Sans SemiBold */}
-      <Font
-        fontFamily="Open Sans"
-        fallbackFontFamily="Arial"
-        webFont={{
-          url: 'https://fonts.gstatic.com/s/opensans/v40/mem5YaGs126MiZpBA-UNirkOUuhp.woff2',
-          format: 'woff2',
-        }}
-        fontWeight={600}
-        fontStyle="normal"
+      <link
+        href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
+        rel="stylesheet"
       />
     </Head>
   );
